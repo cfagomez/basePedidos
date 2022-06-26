23ca50d8ab93fd9c645b186393cc0a0ee2035baa
Federico <cuervog992@gmail.com>
2022-06-26 18:58:35 -0300

    buscar pedido pagina

--- src/paginas/BuscarPedido.jsx
+++ src/paginas/BuscarPedido.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 const BuscarPedido = () => {
   return (
-    <div>BuscarPedido</div>
+    <div>
+      <h1 className='font-black text-4xl text-black text-center mb-10'>Buscar Pedidos</h1>
+      <input 
+        type="text"
+        placeholder="Ingrese numero de pedido o nombre de la empresa"
+        className='p-3 w-full'
+      />
+      <input 
+        type="button"
+        value="Buscar Pedido"
+        className='p-3 bg-black text-white w-full'
+      />
+    </div>
   )
 }
 
